303220af1d47e9725d7815fdc84cca1136df291b
Julian <julian-buursink@hotmail.com>
2017-10-09 16:21:52 +0200

    -

--- Werkboek/les6/pe6_4.py
+++ Werkboek/les6/pe6_4.py
@@ -1,5 +1,20 @@
-for getal in range(10):
-   getal += 1
-   for getal2 in range(10):
-       getal2 += 1
-       print('{} x {} = {}'.format(getal,getal2,(getal*getal2)))
+studentencijfers = [[95, 92, 86],[66, 75, 54],[89, 72, 100],[34, 0, 0]]
+def gemiddelde_per_student(studentencijfers):
+    totaal = 0
+    lst = []
+    for i in studentencijfers:
+        for x in i:
+            totaal += x
+        lst.append(totaal/3)
+        totaal = 0
+    return lst
+
+def gemiddelde_van_alle_studenten(studentencijfers):
+    totaal = 0
+    for i in studentencijfers:
+        for x in i:
+            totaal += x
+    return totaal
+
+print(gemiddelde_per_student(studentencijfers))
+print(gemiddelde_van_alle_studenten(studentencijfers))
