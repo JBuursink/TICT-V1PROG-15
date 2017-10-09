9475f6a4eb4e1f0ef709e6ed68e77363980f36b0
Julian <julian-buursink@hotmail.com>
2017-10-09 15:48:13 +0200

    -

---
+++ Werkboek/les4/pe4_6.py
@@ -0,0 +1,11 @@
+letterlijst = ['a','b','c']
+
+def wijzig(letterlijst):
+    letterlijst.clear()
+    letterlijst.append('d')
+    letterlijst.append('e')
+    letterlijst.append('f')
+
+print(letterlijst)
+wijzig(letterlijst)
+print(letterlijst)
