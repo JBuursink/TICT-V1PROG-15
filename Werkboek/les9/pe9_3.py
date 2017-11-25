a90322a508129e0d77bf2ff6314aad2fb746098e
JBuursink <31792727+JBuursink@users.noreply.github.com>
2017-11-25 14:36:37 +0100

    -

---
+++ Werkboek/les9/pe9_3.py
@@ -0,0 +1,12 @@
+lines (11 sloc)  379 Bytes
+import csv
+with open('gamers.csv','r')as myCSVFile:
+    reader = csv.reader(myCSVFile,delimiter=';')
+    maxscore = 0
+    for row in reader:
+        score = int(row[2])
+        if score > maxscore:
+            maxscore = score
+            maxnaam = row[0]
+            maxdatum = row[1]
+    print ('De hoogste score is: {} op {} behaald door {}'.format(maxscore,maxdatum,maxnaam))
